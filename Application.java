4a54d96874231bd4fc9a00ae24cf55620455fbbc
cubeiplkumar <emailanilkumarm@gmail.com>
2024-12-21 10:09:20 +0530

    feature202 changes by Ana Developer

--- Application.java
+++ Application.java
@@ -24,4 +24,23 @@ class Main
         c.calculateArea(); //invokes calculateArea() with object c
         c.calculateCircumference(); //invokes calculateCircumference() with object c
     }
+	#feature202 changes by Ana Developer for User Module in oct 2024
+	public class StrategyFactory implements InitializingBean {
+    @Autowired
+    private List<Strategy> strategyList;
+    private Map<String, Strategy> strategyMap;
+    @Override
+    public void afterPropertiesSet() {
+        if (CollectionUtils.isNotEmpty(strategyList)) {
+            int size = (int) Math.ceil(strategyList.size() * 4.0 / 3);
+            Map<String, Strategy> map = new ConcurrentHashMap<>(size);
+            for (Strategy strategy : strategyList) {
+                map.put(strategy.getType(), strategy);
+            }
+            strategyMap = Collections.unmodifiableMap(map);
+        }
+    }
+    ...
+}
+#feature202 changes done
 }
